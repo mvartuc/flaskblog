ad13a96d139218610d3204ce5a314c43f862f548
mvartuc <dev.mvartuc@gmail.com>
2024-07-13 18:13:19 +0200

    changed run.py for deployment

--- run.py
+++ run.py
@@ -6,10 +6,6 @@ app = create_app()
 
 app.debug = True
 
-# ------------------- Live reload ----------------------------------------------
-all_css_files_path = os.path.join(app.static_folder, "*.css")
-all_html_files_path = os.path.join(app.root_path, app.template_folder, "*.html")
-# ------------------- Live reload End ------------------------------------------
 
 @app.shell_context_processor
 def make_shell_context():
@@ -17,14 +13,5 @@ def make_shell_context():
 
 if __name__ == "__main__":
     # Uncomment line below and delete the livereload related code on production
-    #app.run(debug=True)
+    app.run(debug=True)
 
-    # ------------------- Live reload -------------------------
-    server = Server(app.wsgi_app)
-    # watch all .css files for change
-    server.watch(all_css_files_path)
-    # watch all .css files for change
-    server.watch(all_html_files_path)
-    server.serve()
-    # ------------------- Live reload End ---------------------
-    
